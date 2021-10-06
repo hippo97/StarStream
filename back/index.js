122cf0e0eb041b60456400082f419df0b7d20479
hippo97 <83958296+hippo97@users.noreply.github.com>
2021-10-07 00:27:15 +0900

    query test
    쿼리를 테스트해봄
    나중에 내가 원하는 쿼리로 바꿀 예정

--- back/index.js
+++ back/index.js
@@ -6,6 +6,18 @@ const {
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipment]
+    supplies: [Supply]
+  }
+  type Mutation {
+    deleteEquipment(id: String): Equipment
+    insertEquipment(
+      id: String
+      used_by: String
+      count: Int
+      new_or_used: String
+    ): Equipment
   }
   type Team {
     id: Int
@@ -15,12 +27,67 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
   }
 `;
 
 const resolvers = {
   Query: {
-    teams: () => database.teams,
+    teams: () =>
+      database.teams.map((team) => {
+        team.supplies = database.supplies.filter(
+          (supply) => {
+            return supply.team === team.id;
+          }
+        );
+        return team;
+      }),
+    team: (parent, args, context, info) =>
+      database.teams.filter((team) => {
+        return team.id === args.id;
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies,
+  },
+  Mutation: {
+    insertEquipment: (
+      parent,
+      args,
+      context,
+      info
+    ) => {
+      database.equipments.push(args);
+      return args;
+    },
+    deleteEquipment: (
+      parent,
+      args,
+      context,
+      info
+    ) => {
+      const deleted = database.equipments.filter(
+        (equipment) => {
+          return equipment.id === args.id;
+        }
+      )[0];
+      database.equipments =
+        database.equipments.filter(
+          (equipment) => {
+            return equipment.id !== args.id;
+          }
+        );
+      return deleted;
+    },
   },
 };
 

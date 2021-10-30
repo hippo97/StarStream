8e1510acbbcfbb86dff16f93090fa70209a05373
hippo97 <83958296+hippo97@users.noreply.github.com>
2021-10-30 23:38:49 +0900

    Register
    회원가입 구현 중

---
+++ front/pages/register.tsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "@apollo/client";
+import { useForm } from "../utils/hooks";
+
+function Register(props: any) {
+  const [errors, setErrors] = useState({});
+
+  const { onChange, onSubmit, values } = useForm(registerUser, {
+    email: "",
+    password: "",
+  });
+
+  const [addUser, { loading }] = useMutation(REGISTER_USER, {
+    update(_, result) {
+      props.history.push("/");
+    },
+    onError(err) {
+      setErrors(err.graphQLErrors[0].extensions.exception.errors);
+    },
+    variables: values,
+  });
+
+  function registerUser() {
+    addUser();
+  }
+
+  return (
+    <div className="form-container">
+      <form onSubmit={onSubmit} className={loading ? "loading" : ""}>
+        <h1>Register</h1>
+        <input
+          label="Username"
+          placeholder="Username.."
+          name="username"
+          type="text"
+          value={values.username}
+          error={errors.username ? true : false}
+          onChange={onChange}
+        />
+        <input
+          label="Email"
+          placeholder="Email.."
+          name="email"
+          type="email"
+          value={values.email}
+          error={errors.email ? true : false}
+          onChange={onChange}
+        />
+        <input
+          label="Password"
+          placeholder="Password.."
+          name="password"
+          type="password"
+          value={values.password}
+          error={errors.password ? true : false}
+          onChange={onChange}
+        />
+        <input
+          placeholder="Confirm Password.."
+          name="confirmPassword"
+          type="password"
+          value={values.confirmPassword}
+          onChange={onChange}
+        />
+        <button type="submit">Register</button>
+      </form>
+      {Object.keys(errors).length > 0 && (
+        <div className="ui error message">
+          <ul className="list">
+            {Object.values(errors).map((value) => (
+              <li key={value}>{value}</li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+const REGISTER_USER = gql`
+  mutation createUser($email: String!, $password: String!) {
+    register(registerInput: { email: $email, password: $password }) {
+      email
+    }
+  }
+`;
+
+export default Register;
